419b13ceef9847c1f0530636761af4b9795babb6
greenteasocha <kouheiatts@gmail.com>
2019-05-04 05:25:47 +0900

    max手順を出力するところまで

--- script/readalgs_max.py
+++ script/readalgs_max.py
@@ -1,33 +1,52 @@
 import pandas as pd
 import json
+import re
 
-with open("../data/convert.json", "r", encoding="utf-8") as fcv:
-    convert = json.load(fcv)
 
-df = pd.read_csv("../data/GrahamUF.csv")
+def main():
+    with open("../data/convert.json", "r", encoding="utf-8") as fcv:
+        convert = json.load(fcv)
 
-df.index = df["first/last"]
-df = df.drop("first/last",axis=1)
+    df = pd.read_csv("../data/MaxFU.csv")
+    regex_colname = re.compile(r"[UDLRFB][UDLRFB]")
+    base_col_name = df.columns[0]
+    df.index = df[base_col_name]
 
-#一度"BU"などの形にする
-df_new = df.rename(columns=lambda s: s[3:5],
-                 index=lambda s: s[3:5],
-                 inplace=True)
-#さらにひらがなにする
-df_kana = df.rename(columns=lambda s: convert[s],
-                    index=lambda s: convert[s],
-                    inplace=True)
-numbering_list = "あいうえかきくけさしすせたちつてなにぬねはひふへ"
-algs = {}
-print(df.loc["い"]["え"])
-for x in numbering_list:
-    for y in numbering_list:
-        try:
-            algs[x+y] = df.loc[x][y]
-        except:
-            pass
+    # drop unused columns
+    df.drop((i for i in df.columns if not regex_colname.search(i))
+            , axis=1
+            , inplace=True)
 
-print(algs["はけ"])
-print(algs["へな"])
-with open("../data/myalgs.json", "w", encoding="utf-8") as fj:
-    json.dump(algs, fj, ensure_ascii=False, indent=4)
+    # drop unused rows
+    df.drop((index for index in df.index if not regex_colname.search(index))
+            , axis=0
+            , inplace=True)
+
+    # 一度"BU"などの形にする
+    df_new = df.rename(columns=lambda s: regex_colname.search(s)
+                       , index=lambda s: regex_colname.search(s))
+
+    # さらにひらがなにする
+    # FUをUFに直している
+    df_kana = df.rename(columns=lambda s: convert[s[::-1]],
+                        index=lambda s: convert[s[::-1]],
+                        inplace=True)
+
+    numbering_list = "あいうえかきくけさしすせたちつてなにぬねはひふへ"
+    algs = {}
+    for x in numbering_list:
+        for y in numbering_list:
+            try:
+                # ここものすごくpythonっぽい書き方で怖い x:str, y:str
+                # 都合上逆にしています
+                algs[x + y] = df.loc[y][x]
+            except:
+                algs[x + y] = "NaN"
+
+    print(algs["はけ"])
+    print(algs["へな"])
+    with open("../data/myalgs.json", "w", encoding="utf-8") as fj:
+        json.dump(algs, fj, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    main()
